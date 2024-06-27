fadd4faf81a5e123b3146468c3d72bd2c11b5cf8
Hunter2828HHgit <haciyevhuseyn2008@gmail.com>
2024-06-27 15:47:36 +0300

    Добавьте файлы проекта.

---
+++ Client/Client.cpp
@@ -0,0 +1,69 @@
+﻿#include <bits/stdc++.h>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread.hpp>
+
+boost::asio::io_context io;
+boost::asio::ip::tcp::socket sock(io);
+char read_buf[1024], write_buf[1024];
+
+void Write();
+void Read();
+
+void connection_handler(const boost::system::error_code& ec) {
+    if (!ec) {
+        Write();
+    }
+    else {
+        std::cerr << "Connection error: " << ec.message() << '\n';
+    }
+}
+
+void read_handler(const boost::system::error_code& ec, std::size_t bytes) {
+    if (!ec) {
+        std::cout << "Server: " << std::string(read_buf, bytes) << '\n';
+        Write();
+    }
+    else {
+        std::cerr << "Client read error: " << ec.message() << '\n';
+    }
+}
+
+void write_handler(const boost::system::error_code& ec, std::size_t /*bytes*/) {
+    if (!ec) {
+        Read();
+    }
+    else {
+        std::cerr << "Client write error: " << ec.message() << '\n';
+    }
+}
+
+void Read() {
+    sock.async_read_some(boost::asio::buffer(read_buf),
+        [](const boost::system::error_code& ec, std::size_t bytes) {
+            read_handler(ec, bytes);
+        });
+}
+
+void Write() {
+    std::string msg;
+    std::getline(std::cin, msg);
+    std::fill(std::begin(write_buf), std::end(write_buf), 0);
+    std::copy(msg.begin(), msg.end(), write_buf);
+    boost::asio::async_write(sock, boost::asio::buffer(write_buf, msg.size()),
+        [](const boost::system::error_code& ec, std::size_t bytes) {
+            write_handler(ec, bytes);
+        });
+}
+
+int main() {
+    try {
+        boost::asio::ip::tcp::endpoint ep(boost::asio::ip::make_address("127.0.0.1"), 9001);
+        sock.async_connect(ep, connection_handler);
+        io.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << '\n';
+    }
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,66 @@
+﻿#include <bits/stdc++.h>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include<boost/function.hpp>
+
+
+class Server {
+public:
+
+	Server(boost::asio::io_context& io, short port) : 
+		io_(io), acceptor_(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)), sock_(io){}
+	
+	void start_accept() {
+		acceptor_.async_accept(sock_, 
+			boost::bind(&Server::connection_handler, this, boost::asio::placeholders::error));
+	}
+private:
+	void connection_handler(const boost::system::error_code &ec) {
+		if (!ec) {
+			std::cout << "connected\n";
+			Read();
+		}
+		start_accept();
+	}
+
+	void Read() {
+		sock_.async_read_some(boost::asio::buffer(data_), 
+			boost::bind(&Server::read_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));		
+	}
+
+	void Write(size_t bytes) {
+		sock_.async_write_some(boost::asio::buffer(data_, bytes),
+			boost::bind(&Server::write_handler, this, boost::asio::placeholders::error));
+	}
+
+	void read_handler(const boost::system::error_code &ec, size_t bytes) {
+		if (!ec) {
+			std::string received(data_, bytes);
+			std::cout << "received: " << received<<'\n';
+			Write(bytes);
+		}
+	}
+
+	void write_handler(const boost::system::error_code &ec) {
+		if (!ec) Read();
+	}
+private:
+	boost::asio::io_context& io_;
+	boost::asio::ip::tcp::acceptor acceptor_;
+	boost::asio::ip::tcp::socket sock_;
+	enum {max_len = 1024};
+	char data_[max_len];
+};
+
+int main() {
+	try {
+		boost::asio::io_context io;
+		Server server(io, 9001);
+		server.start_accept();
+		io.run();
+	}
+	catch (std::exception e) {
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
+}
